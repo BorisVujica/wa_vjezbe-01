d874041f7d203821aba6298e7700193a3aee51a5
boris_vujica <borisvujica7@gmail.com>
2024-10-28 19:47:12 +0100

    samostalni zadatak za vjezbu1

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express=require("express");
-
 const app=express();
-
 app.get("/",(req,res)=>{
-    res.send("hello world")
+    res.sendFile(__dirname+"/public/index.html");
+
 });
 
+app.get("/about",(req,res)=>{
+    res.sendFile(__dirname+"/public/about.html");
+
+});
+
+app.get("/users",(req,res)=>{
+    const users=[
+        {id:1,ime:"boris",prezime:"vujica"},
+        {id:2,ime:"tomislav",prezime:"perkovic"},
+        {id:3,ime:"josip",prezime:"ban jelacic"},
+    ];
+    res.json(users);
+
+});
 const port=3000;
 app.listen(port,(error)=>{;
     if (error){
